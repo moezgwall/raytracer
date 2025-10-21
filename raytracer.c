ddfeafb660ee4775e48ceec2ff6d845bd4565e49
moez chroufa <chroufa.moezkhan@gmail.com>
2025-10-21 09:56:20 +0200

    template for raytracer.h

--- raytracer.c
+++ raytracer.c
@@ -7,76 +7,69 @@
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "stb_image_write.h"
 
+#define RAYTRACER_IMPLEMENTATION
+#include "raytracer.h"
+
 #define SCR_WIDTH 800
 #define SCR_HEIGHT 600
 
-typedef struct {
-    Vec3f origin;
-    Vec3f direction;
-} Ray;
-
-typedef struct {
-    Vec3f center;
-    float radius;
-} Sphere;
-
-// Ray-sphere intersection
-bool doIntersect(Ray ray, Sphere sp, float *t) {
-    Vec3f oc = vec3f_sub(ray.origin, sp.center);
-    float a = vec3f_dot(ray.direction, ray.direction);
-    float b = 2.0f * vec3f_dot(oc, ray.direction);
-    float c = vec3f_dot(oc, oc) - sp.radius * sp.radius;
-    float discriminant = b * b - 4 * a * c;
-    if (discriminant < 0) return false;
-    float discrSQRT = sqrtf(discriminant);
-    float t0 = (-b - discrSQRT) / (2 * a);
-    float t1 = (-b + discrSQRT) / (2 * a);
-    *t = (t0 < t1) ? t0 : t1;
-    return true;
-}
-
+// template for raytracer.h 
+// author : gWall
+// date : 10/21/2025
 int main() {
     unsigned char *image = malloc(SCR_WIDTH * SCR_HEIGHT * 3);
     if (!image) {
-        fprintf(stderr, "Image buffer allocation failed\n");
+        fprintf(stderr, "Image allocation failed\n");
         return 1;
     }
 
-    Vec3f origin = {0, 0, 0};
-    Vec3f lookAt = {0, 0, -1};
-    Vec3f up = {0, 1, 0};
-    float fov = 90.0f; // 90.0f
-    float ar = (float)SCR_WIDTH / (float)SCR_HEIGHT;
-    float scale = tanf((fov * 0.3f) * LAMATH_PI / 180.0f);
+    // Scene config
+    Vec3f camPos = {0, 0, 0};
+    Vec3f camTarget = {0, 0, -1};
+    Vec3f camUp = {0, 1, 0};
+    float fov = 90.0f;
+    float aspect = (float)SCR_WIDTH / (float)SCR_HEIGHT;
+    Camera cam = makeCamera(camPos, camTarget, camUp, fov, aspect);
 
-    Vec3f forward = vec3f_normalize(vec3f_sub(lookAt, origin));
-    Vec3f right = vec3f_normalize(vec3f_cross(forward, up));
-    Vec3f nup = vec3f_cross(right, forward);
-
-    Sphere sphere = {{0, 0, -5}, 1};
+    // set the light vector
     Vec3f light = {55, 15, -10};
 
+    // the objects tp render in the scene 
+    Sphere sphere = {{0, 0, -5}, 1};
+    Plane plane = {{0, -1, 0}, {0, 1, 0}};  // horizontal plane at y = -1
+    
+    Hittable objects[2];
+    objects[0] = makeSphere(&sphere);
+    objects[1] = makePlane(&plane);
+    int objectCount = 2;
+    // render to the screen
     for (int y = 0; y < SCR_HEIGHT; y++) {
         for (int x = 0; x < SCR_WIDTH; x++) {
-            float px = (2 * ((x + 0.5f) / (float)SCR_WIDTH) - 1) * ar * scale;
-            float py = (1 - 2 * ((y + 0.5f) / (float)SCR_HEIGHT)) * scale;
-            Vec3f ray_dir = vec3f_add(vec3f_add(vec3f_scale(right, px), vec3f_scale(nup, py)), forward);
-            ray_dir = vec3f_normalize(ray_dir);
+            Ray ray = generateRay((float)x, (float)y, SCR_WIDTH, SCR_HEIGHT, cam);
+            float closestT = INFINITY;
+            Hittable *hitObject = NULL;
+            Vec3f hitPoint, normal;
 
-            Ray ray = {origin, ray_dir};
-            float t;
+            for (int i = 0; i < objectCount; i++) {
+                float t;
+                if (objects[i].doIntersect(ray, objects[i].data, &t)) {
+                    if (t < closestT) {
+                        closestT = t;
+                        hitObject = &objects[i];
+                    }
+                }
+            }
+
             int idx = (y * SCR_WIDTH + x) * 3;
-
-            if (doIntersect(ray, sphere, &t)) {
-                Vec3f hit_point = vec3f_add(ray.origin, vec3f_scale(ray.direction, t));
-                Vec3f normal = vec3f_normalize(vec3f_sub(hit_point, sphere.center));
-                Vec3f light_dir = vec3f_normalize(vec3f_sub(light, hit_point));
-                float diffuse = fmaxf(0, vec3f_dot(normal, light_dir));
-                int intensity = (int)(diffuse * 255);
-                image[idx + 0] = intensity;
-                image[idx + 1] = intensity;
-                image[idx + 2] = intensity;
+            if (hitObject) {
+                hitPoint = vec3f_add(ray.origin, vec3f_scale(ray.direction, closestT));
+                normal = hitObject->getNormal(hitPoint, hitObject->data);
+                Vec3f color = computeLighting(hitPoint, normal, light);
+                image[idx + 0] = (unsigned char)(fminf(color.x, 1.0f) * 255);
+                image[idx + 1] = (unsigned char)(fminf(color.y, 1.0f) * 255);
+                image[idx + 2] = (unsigned char)(fminf(color.z, 1.0f) * 255);
             } else {
+                // Background color
                 image[idx + 0] = 20;
                 image[idx + 1] = 20;
                 image[idx + 2] = 20;
@@ -86,6 +79,7 @@ int main() {
 
     stbi_write_png("output.png", SCR_WIDTH, SCR_HEIGHT, 3, image, SCR_WIDTH * 3);
     free(image);
-    printf("Rendered image saved to output.png\n");
+    printf("Image rendered to output.png\n");
     return 0;
 }
+
